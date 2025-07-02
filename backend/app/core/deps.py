d6463b5a0465e4d89d0f5963d8bdbd099b6e5c48
Priyanshu3369 <priyanshuchourasiya32198@gmail.com>
2025-07-02 11:54:26 +0530

    authorization done

---
+++ backend/app/core/deps.py
@@ -0,0 +1,33 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+from ..db.mongo import db
+from bson import ObjectId
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id: str = payload.get("sub")
+        if user_id is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+
+    user = await db.users.find_one({"_id": ObjectId(user_id)})
+    if user is None:
+        raise credentials_exception
+    return user
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI , Depends
 from .api.routes import auth
+from .core.deps import get_current_user
 
 app = FastAPI()
 
@@ -8,3 +9,7 @@ app.include_router(auth.router)
 @app.get("/")
 def root():
     return {"message": "FinWatch API is running ✅"}
+
+@app.get("/protected")
+async def protected_route(current_user: dict = Depends(get_current_user)):
+    return {"message": f"Hello, {current_user['email']}! 🔒 You are authenticated."}
